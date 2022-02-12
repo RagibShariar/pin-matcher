51f72f3cf4776cf25ecc2e21e01b0d923a1b7488
RagibShariar <srragib@gmail.com>
2022-02-13 03:20:07 +0600

    verify pin added

--- js/app.js
+++ js/app.js
@@ -1,17 +1,61 @@
-function getPin(){
-    const pin = Math.round(Math.random()*10000);
+function getPin() {
+    const pin = Math.round(Math.random() * 10000);
     const pinText = pin + '';
     if (pinText.length > 3) {
         return pin;
     }
-    else{
+    else {
         // console.log('got 3 digit and calling again', pin);
         return getPin();
     }
 };
 
-function generatePin(){
+function generatePin() {
     const pin = getPin();
     document.getElementById('pin-display').value = pin;
     // console.log(pin);
 };
+
+
+
+document.getElementById('keypad-body').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+
+
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        if (number == 'C') {
+            calcInput.value = '';
+        }
+    }
+    else {
+        // get the input field
+
+        const prevNumber = calcInput.value;
+        const newNumber = prevNumber + number;
+
+        // set the numbers into input field
+        calcInput.value = newNumber;
+    }
+
+});
+
+//verifying pin
+function verifyPin() {
+    const successMessage = document.getElementById('success-msg');
+    const errorMessage = document.getElementById('error-msg');
+
+    const pin = document.getElementById('pin-display').value;
+    const calcInput = document.getElementById('typed-numbers').value;
+
+
+
+    if (pin == calcInput) {
+        successMessage.style.display = "block";
+        errorMessage.style.display = "none";
+    }
+    else {
+        errorMessage.style.display = "block";
+        successMessage.style.display = "none";
+    }
+}
